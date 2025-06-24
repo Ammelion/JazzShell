d665c5de227a307d0f6cffa2155fa07addc995d6
Ammelion <ojas1030@gmail.com>
2025-06-24 23:09:53 +0530

    .

--- src/main.c
+++ src/main.c
@@ -4,9 +4,11 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+  
   // Uncomment this block to pass the first stage
   printf("$ ");
+  fgets(input,100,stdin);
+  printf("%s: command not found\n", input)
 
   // Wait for user input
   char input[100];
