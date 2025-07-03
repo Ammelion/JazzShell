1d5539b706539762f8c1041f8c6fe04dd602b482
Ammelion <ojas1030@gmail.com>
2025-07-04 00:04:33 +0530

    Insane tab completions

--- src/main.c
+++ src/main.c
@@ -6,14 +6,116 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <termios.h>
 
-int parser(char *input, char *args[]){
+typedef struct trienode //enables awesome lookup time
+{
+    struct trienode *children[256];
+    bool terminal;
+} trienode;
+
+struct termios orig, raw; //autocompletion stuff
+
+trienode *createnode(){ 
+    trienode *newnode = malloc(sizeof *newnode);
+    for(int i=0;i<256;i++){
+        newnode->children[i]=NULL;
+    }
+    newnode->terminal=false;
+    return newnode;
+}
+
+bool trieinsert(trienode **root, char *signedtext){
+    if (*root==NULL){
+        *root=createnode();
+    }
+
+    unsigned char *text=(unsigned char *)signedtext;
+    trienode *tmp=*root;
+    int length=strlen(signedtext);
+    for(int i=0;i<length;i++){
+        if(tmp->children[text[i]]==NULL){
+            tmp->children[text[i]]=createnode();
+        }
+        tmp=tmp->children[text[i]];
+    }
+    if(tmp->terminal){
+        return false;
+    }
+    else{
+        tmp->terminal = true;
+        return true;
+    }
+}
+
+trienode *cptrie(trienode *root){
+    if(!root){return NULL;}
+
+    trienode *newnode=createnode();
+    newnode->terminal=root->terminal;
+
+    for(int i=0; i<256; i++){
+        if(root->children[i]){
+            newnode->children[i]=cptrie(root->children[i]);
+        }
+    }
+    
+    return newnode;
+}
+
+trienode *find(trienode *root,const char *prefix){
+    trienode *node=root;
+    for (;*prefix && node;prefix++) {
+        node = node->children[(unsigned char)*prefix];
+    }
+    return node;
+}
+
+static int dfs(trienode *node, char *buf, int depth) {
+    int found = 0;
+    if (node->terminal) {
+        buf[depth] = '\0';
+        found = 1;
+    }
+    for (int c = 0; c < 256 && found < 2; c++) {
+        if (node->children[c]) {
+            buf[depth] = (char)c;
+            int n = dfs(node->children[c], buf, depth + 1);
+            found += n;
+            if (found > 1) break;
+        }
+    }
+    return found;
+}
+
+bool trie_unique_suffix(trienode *node, char *outbuf) {
+    int count = dfs(node, outbuf, 0);
+    return (count == 1);
+}
+
+void enable_raw_mode(){
+    tcgetattr(STDIN_FILENO, &orig);
+    raw = orig;
+    raw.c_iflag &= ~(BRKINT|INPCK|ISTRIP|IXON);
+    //raw.c_oflag &= ~OPOST; //Fucking Trash flag
+    raw.c_cflag |= CS8;
+    raw.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG);
+    raw.c_cc[VMIN] = 1;
+    raw.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disable_raw_mode(){
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig);
+}
+
+int parser(char *input, char *args[]){  //this is the funny tokenizaton part, checks for all backslashes , quoting and what not
     int count=0,single=0,idouble=0;
     char *read=input,*write=input,*token=NULL;
-
     while(*read){
         char c=*read;
-
         if(single){
             if (c=='\''){
                 single=0;
@@ -86,7 +188,7 @@ int parser(char *input, char *args[]){
     return count;
 }
 
-int runexec(char **arr, int stream, char *red_op, char *red_file){
+int runexec(char **arr, int stream, char *red_op, char *red_file){ //if i "somehow" fail to find a builtin... maybe its in the executables list
     int found=0;
     char command[100];
     strcpy(command,arr[0]);
@@ -132,18 +234,83 @@ int runexec(char **arr, int stream, char *red_op, char *red_file){
     }
 }
 
+ssize_t read_line(char *buf, size_t size, trienode *groot) {
+    size_t pos=0;
+    while(1){
+        char c;
+        if (read(STDIN_FILENO,&c,1)<=0) return -1;
+        if (c=='\n' || c == '\r') {
+            write(STDOUT_FILENO, "\r\n", 2);
+            break;
+        }
+        else if(c=='\t'){
+            buf[pos] = '\0';
+            trienode *n = find(groot, buf);
+            char suffix[100];
+            if (n && trie_unique_suffix(n, suffix)) {
+                int add = strlen(suffix);
+                if (pos + add + 1 < size) {
+                    memcpy(buf + pos, suffix, add);
+                    pos += add;
+                    buf[pos++] = ' ';
+                    write(STDOUT_FILENO, suffix, add);
+                    write(STDOUT_FILENO, " ", 1);
+                }
+            }
+        }
+        else if(c==127 || c=='\b'){
+            if (pos > 0){
+                pos--;
+                write(STDOUT_FILENO, "\b \b", 3);
+            }
+        } else if (isprint((unsigned char)c)){
+            if(pos + 1 < size){
+                buf[pos++] = c;
+                write(STDOUT_FILENO, &c, 1);
+            }
+        }
+    }
+    buf[pos] = '\0';
+    return pos;
+}
+
+
 int main(void){
     setbuf(stdout,NULL);
-    char *args[100];
-    while(1){
-        printf("$ ");
-        char input[100];
-        if(!fgets(input,100,stdin)) break;
-        input[strcspn(input,"\n")] = '\0';
-        int nargs = parser(input,args);
-        if(nargs==0) continue;
-        char *cmd = args[0];
+    char *args[100],len=0;
+    trienode *sroot =NULL;
+    char *builtins[]={"echo","exit","type","pwd","cd",NULL};
+    while(builtins[len]){len++;}
 
+    for(int i=0;i<len;i++){
+        trieinsert(&sroot,builtins[i]);
+    }
+    enable_raw_mode();
+    while(1){
+        //i MUST initiate the stupid autocompletion part ;p
+        trienode *groot=cptrie(sroot);
+        DIR *d=opendir(".");
+        if(d){
+            struct dirent *ent;
+            while(ent=readdir(d)){
+                if(ent->d_name[0]=='.') continue;
+                trieinsert(&groot,ent->d_name);
+            }
+            closedir(d);
+        }
+        write(STDOUT_FILENO, "\r$ ", 3);
+        char input[100];
+        read_line(input,sizeof input, groot);
+        int nargs = parser(input,args);
+
+        disable_raw_mode();
+
+        if(nargs==0){
+            enable_raw_mode();
+            continue;
+        }
+
+        char *cmd = args[0];
         int stream=-1, index=-1;
         char *red_file=NULL, *red_op=NULL;
         for(int i=0; args[i]; i++){
@@ -225,7 +392,6 @@ int main(void){
         }
         else if(strcmp(cmd,"type")==0){
             char *t = nargs>1 ? args[1] : NULL;
-            char *builtins[]={"echo","exit","type","pwd","cd"};
             int fb=0;
             for(int i=0;i<5;i++){
                 if(t&&strcmp(t,builtins[i])==0){
@@ -262,6 +428,8 @@ int main(void){
             dup2(builtin_saved,stream);
             close(builtin_saved);
         }
+        enable_raw_mode();
     }
+    disable_raw_mode();
     return 0;
 }
