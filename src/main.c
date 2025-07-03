7336250904bf4f35e969a5d3262d6b37ba737774
Ammelion <ojas1030@gmail.com>
2025-07-04 01:37:42 +0530

    .

--- src/main.c
+++ src/main.c
@@ -279,7 +279,7 @@ ssize_t read_line(char *buf, size_t size, trienode *groot) {
 }
 
 int main(void){
-    printf("$ ");
+    write(STDOUT_FILENO, "\r$ ", 3);
     setbuf(stdout,NULL);
     char *args[100],len=0;
     trienode *sroot =NULL;
